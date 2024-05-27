473ee88c698819e407725dbecbc6ca90dfe8f001
jamalihassan0307 <jamalihassan0307@gmail.com>
2024-05-27 22:05:43 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:ecommers_app/pages/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Main());
+}
+
+class Main extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
