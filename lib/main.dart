e96becb87ba5848bfe55b4d3920c88221731f019
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-05 10:26:51 +0500

    Refactor main.dart: initialize database connection, switch to GetMaterialApp for routing, and add new screens for login, signup, and profile settings to enhance navigation and user experience.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,39 @@
 import 'package:ecommers_app/pages/HomePage.dart';
 import 'package:ecommers_app/screens/welcome_screen.dart';
+import 'package:ecommers_app/screens/login_screen.dart';
+import 'package:ecommers_app/screens/signup_screen.dart';
+import 'package:ecommers_app/screens/profile_settings_screen.dart';
 import 'package:ecommers_app/services/sqlService.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main(){
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SQLService sqlService = SQLService();
+  await sqlService.openDB();
   runApp(const Main());
-    SQLService sqlService = SQLService();
-  sqlService.openDB();
-} 
+}
 
-class Main extends StatelessWidget{
+class Main extends StatelessWidget {
   const Main({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
+      title: 'Shopping App',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => const WelcomeScreen()),
+        GetPage(name: '/login', page: () => const LoginScreen()),
+        GetPage(name: '/signup', page: () => const SignupScreen()),
+        GetPage(name: '/home', page: () => HomePage()),
+        GetPage(name: '/profile', page: () => const ProfileSettingsScreen()),
+      ],
     );
   }
 }
