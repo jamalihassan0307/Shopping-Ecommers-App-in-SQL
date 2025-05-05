bee21acd10186ed56203faf8269dd3e6c2cc9049
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-05 14:00:10 +0500

    Fix import statement in SharedPrefsService to ensure correct package usage for shared preferences functionality.

--- lib/services/shared_prefs_service.dart
+++ lib/services/shared_prefs_service.dart
@@ -1,4 +1,5 @@
-import 'package:shared_preferences.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPrefsService {
   static const String _userIdKey = 'user_id';
