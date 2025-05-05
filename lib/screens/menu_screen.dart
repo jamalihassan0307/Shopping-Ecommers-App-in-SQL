1ab824e3219f325b509e48aa894a52830f8a0994
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-05 10:48:58 +0500

    Add image asset: include new image.jpg for use in MenuScreen background.

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -25,8 +25,8 @@ class MenuScreen extends StatelessWidget {
               // Avatar with animation
               CircleAvatar(
                 radius: 40,
-                backgroundImage: NetworkImage(
-                  "https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+                backgroundImage: AssetImage(
+                  "images/image.jpg",
                 ),
               ).animate()
                 .scale(duration: 600.ms)
