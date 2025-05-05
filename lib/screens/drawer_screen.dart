4a83a7624db6d3c3fde86fa8773f6b026960a62d
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-05 10:39:55 +0500

    Add DrawerScreen: implement a new drawer interface using ZoomDrawer for navigation, integrating MenuScreen and MainScreen with enhanced user interaction and styling options.

---
+++ lib/screens/drawer_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'menu_screen.dart';
+import 'main_screen.dart';
+
+class DrawerScreen extends StatefulWidget {
+  const DrawerScreen({Key? key}) : super(key: key);
+
+
+
+  @override
+  State<DrawerScreen> createState() => _DrawerScreenState();
+}
+
+class _DrawerScreenState extends State<DrawerScreen> {
+  final ZoomDrawerController _drawerController = ZoomDrawerController();
+
+  @override
+  Widget build(BuildContext context) {
+    return ZoomDrawer(
+      controller: _drawerController,
+      menuScreen: MenuScreen(
+        onItemSelected: () {
+          _drawerController.toggle!();
+        },
+      ),
+      mainScreen: MainScreen(
+        onMenuPressed: () {
+          _drawerController.toggle!();
+        },
+      ),
+      borderRadius: 24.0,
+      showShadow: true,
+      angle: -12.0,
+      slideWidth: MediaQuery.of(context).size.width * 0.65,
+      menuBackgroundColor: Colors.deepPurple.shade700,
+      mainScreenScale: 0.2,
+      mainScreenTapClose: true,
+      menuScreenWidth: MediaQuery.of(context).size.width * 0.65,
+      openCurve: Curves.easeInOut,
+      closeCurve: Curves.easeInOut,
+      duration: const Duration(milliseconds: 300),
+    );
+  }
+} 
