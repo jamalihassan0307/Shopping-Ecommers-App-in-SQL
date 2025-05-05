b5f48abb4624cb5c43aaffc7fe55c9aee4da2dc9
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-05 10:40:05 +0500

    Refactor MainScreen constructor: update to use named parameters for improved clarity and consistency, and remove redundant import statement.

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,12 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 
 class MainScreen extends StatelessWidget {
   final VoidCallback onMenuPressed;
+  const MainScreen({
+    Key? key,
+    required this.onMenuPressed,
+  }) : super(key: key);
 
-  const MainScreen({super.key, required this.onMenuPressed});
 
   @override
   Widget build(BuildContext context) {
