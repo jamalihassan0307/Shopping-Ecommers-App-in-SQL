9ba691094fdb5d5685c98327c0bcee3b49f1cecb
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-05 09:56:04 +0500

    Refactor Main widget: update Main constructor to be constant and adjust widget test to reflect this change for improved performance and consistency.

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,16 @@ import 'package:ecommers_app/services/sqlService.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(Main());
+  runApp(const Main());
     SQLService sqlService = SQLService();
   sqlService.openDB();
 }
 
 class Main extends StatelessWidget{
+  const Main({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
