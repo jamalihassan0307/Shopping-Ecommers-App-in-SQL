10284ae70c5f343e4b0de954b697ec8fe5bd69f3
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-05 10:39:40 +0500

    Refactor main application structure: rename Main to MyApp, update initial route to '/welcome', and integrate Google Fonts for improved typography. Add new routes for Favorites, Cart, and ItemDetail screens to enhance navigation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,43 @@
-import 'package:ecommers_app/pages/HomePage.dart';
-import 'package:ecommers_app/screens/welcome_screen.dart';
-import 'package:ecommers_app/screens/login_screen.dart';
-import 'package:ecommers_app/screens/signup_screen.dart';
-import 'package:ecommers_app/screens/profile_settings_screen.dart';
-import 'package:ecommers_app/services/sqlService.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'screens/drawer_screen.dart';
+import 'screens/welcome_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
+import 'screens/profile_settings_screen.dart';
+import 'screens/favorites_screen.dart';
+import 'pages/CartPage.dart';
+import 'pages/ItemDetail.dart';
+import 'pages/HomePage.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  SQLService sqlService = SQLService();
-  await sqlService.openDB();
-  runApp(const Main());
+void main() {
+  runApp(const MyApp());
 }
 
-class Main extends StatelessWidget {
-  const Main({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      title: 'ShopEase',
       debugShowCheckedModeBanner: false,
-      title: 'Shopping App',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        useMaterial3: true,
       ),
-      initialRoute: '/',
+      initialRoute: '/welcome',
       getPages: [
-        GetPage(name: '/', page: () => const WelcomeScreen()),
+        GetPage(name: '/welcome', page: () => const WelcomeScreen()),
         GetPage(name: '/login', page: () => const LoginScreen()),
         GetPage(name: '/signup', page: () => const SignupScreen()),
-        GetPage(name: '/home', page: () => HomePage()),
+        GetPage(name: '/home', page: () => const DrawerScreen()),
         GetPage(name: '/profile', page: () => const ProfileSettingsScreen()),
+        GetPage(name: '/favorites', page: () => FavoritesScreen()),
+        GetPage(name: '/cart', page: () => CartPage()),
+        GetPage(name: '/item-detail', page: () => const ItemDetailPage(itemId: 0)),
       ],
     );
   }
